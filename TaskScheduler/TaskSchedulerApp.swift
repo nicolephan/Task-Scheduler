a724401ad0ecc13236843efe0fa596ebaf83ca90
Vanessa Huynh <vanehuyn@gmail.com>
2024-10-03 21:35:18 -0400

    chore: add initial app files

---
+++ TaskScheduler/TaskSchedulerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskSchedulerApp.swift
+//  TaskScheduler
+//
+//  Created by Vanessa Huynh on 10/3/24.
+//
+
+import SwiftUI
+
+@main
+struct TaskSchedulerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
